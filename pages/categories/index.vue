f1942bb27aff67e48575087f92634fcfd9ce18f5
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-03-25 14:02:18 +0700

    feat: create add category modal

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -2,20 +2,28 @@
   <v-row class="mt-8" justify="center">
     <v-card class="w-50 text-center rounded">
       <v-card-title>Categories Management</v-card-title>
-      <v-card-text class="mt-4">Type in the input to find category</v-card-text>
     </v-card>
   </v-row>
   <v-row class="mt-8 w-50 mx-auto">
-    <v-text-field
-      label="Search here..."
-      variant="underlined"
-      prepend-icon="mdi-magnify"
+    <v-col><category-add-form /></v-col>
+    <v-col
+      ><nuxt-link to="/categories/search">
+        <v-btn><v-icon class="mr-2">mdi-magnify</v-icon>Search Category</v-btn>
+      </nuxt-link></v-col
     >
-    </v-text-field>
   </v-row>
   <nuxt-child />
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.v-col {
+  display: flex;
+  justify-content: center;
+
+  .v-btn {
+    color: $primary-color;
+  }
+}
+</style>
